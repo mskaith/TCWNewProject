3edc76e41d6d9ab9f5af69b3abdc3d92d8d513e7
Mohit <mohitkaithwas66@gmail.com>
2022-05-30 22:37:50 +0530

    Added PTO page Test Script

--- src/test/java/testcases/Reports.java
+++ src/test/java/testcases/Reports.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 import base.BaseTest;
 
 public class Reports extends BaseTest {
-	
+
 	@Test(priority = 1)
 	public void clickOnReportTab() {
 		WebElement sidebarMenuLst = driver.findElement(By.xpath(OR.getProperty("sidebarMenuLst_xp")));
@@ -21,26 +21,28 @@ public class Reports extends BaseTest {
 		act.moveToElement(reportTabBtn).click().build().perform();
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("divLoading")));
-		//System.out.println("1");
+		// System.out.println("1");
 		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("divLoading")));
-		//System.out.println("2");
+		// System.out.println("2");
 		String reportPageTtl = driver.findElement(By.xpath(OR.getProperty("reportPageTtl_xp"))).getText();
 		Assert.assertEquals(reportPageTtl, "Reports");
 	}
-	@Test(priority=2)
+
+	@Test(priority = 2)
 	public void tsReportPage() {
 		click("tsVReportBtn_xp");
 		driver.switchTo().frame(OR.getProperty("tsVReportFrame_id"));
 		String tsVReportStdTtl = driver.findElement(By.xpath(OR.getProperty("tsVReportStdTtl_xp"))).getText();
 		WebElement tsEmpName = driver.findElement(By.xpath(OR.getProperty("tsEmpName_xp")));
 		wait.until(ExpectedConditions.visibilityOf(tsEmpName));
-		//System.out.println(employee);
+		// System.out.println(employee);
 		Assert.assertEquals(tsVReportStdTtl, "Standard Timesheet Report");
-		
+
 	}
-	@Test(priority=3)
+
+	@Test(priority = 3)
 	public void tsDetailedReportPage() throws InterruptedException {
-		//Thread.sleep(5000);
+		// Thread.sleep(5000);
 		driver.switchTo().defaultContent();
 		WebElement tsDetailedBtn = driver.findElement(By.xpath(OR.getProperty("tsDetailedBtn_xp")));
 		act.moveToElement(tsDetailedBtn).click().build().perform();
@@ -49,8 +51,25 @@ public class Reports extends BaseTest {
 		WebElement tsEmpName = driver.findElement(By.xpath(OR.getProperty("tsEmpName_xp")));
 		wait.until(ExpectedConditions.visibilityOf(tsEmpName));
 		String tsVReportDetailedTtl = driver.findElement(By.xpath(OR.getProperty("tsVReportDetailedTtl_xp"))).getText();
-		Assert.assertEquals(tsVReportDetailedTtl,"Detailed Timesheet Report");
-		
-		
+		Assert.assertEquals(tsVReportDetailedTtl, "Detailed Timesheet Report");
+	}
+
+	@Test(priority = 4)
+	public void ptoBanksPage() {
+		driver.switchTo().defaultContent();
+		WebElement ptoBankTabBtn = driver.findElement(By.xpath(OR.getProperty("ptoBankTabBtn_xp")));
+		act.moveToElement(ptoBankTabBtn).click().build().perform();
+		WebElement ptoBankVReportBtn = driver.findElement(By.xpath(OR.getProperty("ptoBankVReportBtn_xp")));
+		// wait.until(ExpectedConditions.elementToBeClickable(ptoBankVReportBtn));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("divLoading")));
+		// System.out.println("1");
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("divLoading")));
+		// System.out.println("2");
+		click("ptoBankVReportBtn_xp");
+		driver.switchTo().frame(OR.getProperty("ptoBankFrame_id"));
+		WebElement ptoEmpName = driver.findElement(By.xpath(OR.getProperty("ptoEmpName_xp")));
+		wait.until(ExpectedConditions.visibilityOf(ptoEmpName));
+		String ptoBankPageTtl = driver.findElement(By.xpath(OR.getProperty("ptoBankPageTtl_xp"))).getText();
+		Assert.assertEquals(ptoBankPageTtl, "PTO Bank Report");
 	}
 }
